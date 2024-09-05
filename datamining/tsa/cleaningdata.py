a88a691e02100c966cf58c7c5a0de442791c19a0
alexcavadora <49330757+alexcavadora@users.noreply.github.com>
2024-09-04 20:43:49 -0600

    started tsa

--- datamining/tsa/cleaningdata.py
+++ datamining/tsa/cleaningdata.py
@@ -1,8 +1,12 @@
 import pandas as pd
-file = 'dataset/DATASET'
-df_split = [pd.read_csv(file + f"{path}.CSV") for path in range(6)]
-df = pd.concat(df_split, ignore_index=True)
-df['DATE'] = pd.to_datetime(df['DATE'], format='mixed', dayfirst=True)
+file = 'dataset/DATASET.CSV'
+#df_split = [pd.read_csv(file + f"{path}.CSV") for path in range(6)]
+#df = pd.concat(df_split, ignore_index=True)
+df = (pd.read_csv(file))
+df['DATE'] = pd.to_datetime(df['DATE'], format='%d/%m/%Y %H:%M:%S')
 df.set_index('DATE', inplace=True)
 df.sort_index(inplace=True)
+
 print(df)
+df_resampled = df.resample('h').mean()  # Resample by hour and calculate the mean
+print(df_resampled)
